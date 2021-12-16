2e0c2b263d9a06efbd9a3a506a096954002a5fc7
7hens <thenstango@live.com>
2021-12-16 15:59:22 +0800

    chore: upgrade build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,6 @@
 plugins {
-    kotlin("multiplatform") version "1.5.10"
+    kotlin("multiplatform") version "1.6.10"
     `maven-publish`
-    application
 }
 
 group = "com.github.7hens.grule"
@@ -35,22 +34,6 @@ kotlin {
 //            }
 //        }
     }
-    val hostOs = System.getProperty("os.name")
-    val isMingwX64 = hostOs.startsWith("Windows")
-    val nativeTarget = when {
-        hostOs == "Mac OS X" -> macosX64("native")
-        hostOs == "Linux" -> linuxX64("native")
-        isMingwX64 -> mingwX64("native")
-        else -> throw GradleException("Host OS is not supported in Kotlin/Native.")
-    }
-
-    nativeTarget.apply {
-        binaries {
-            executable {
-                entryPoint = "main"
-            }
-        }
-    }
     sourceSets {
         val commonMain by getting
         val commonTest by getting {
@@ -66,16 +49,6 @@ kotlin {
             }
         }
         val jsMain by getting
-        val jsTest by getting {
-            dependencies {
-                implementation(kotlin("test-js"))
-            }
-        }
-        val nativeMain by getting
-        val nativeTest by getting
+        val jsTest by getting
     }
-}
-
-application {
-    mainClass.set("MainKt")
 }
