0649f0a082c66378506ddcbac626286d2e9993e3
7hens <thenstango@live.com>
2022-09-27 20:44:45 +0800

    refactor: SysPrinter

--- src/jsMain/kotlin/io/grule/util/log/SysPrinter.kt
+++ src/jsMain/kotlin/io/grule/util/log/SysPrinter.kt
@@ -1,8 +1,18 @@
 package io.grule.util.log
 
 actual object SysPrinter : Printer {
+    private const val MAX_LENGTH = 1000
+    private const val END_LENGTH = 20
+
     override fun print(level: LogLevel, tag: String, message: Any?) {
         val date = js("new Date().toString()")
-        println("[${level.name.first()}. $date #$tag] $message")
+        println("[${level.name.first()}. $date #$tag] ${shortText(message.toString())}")
+    }
+
+    private fun shortText(text: String): String {
+        if (text.length <= MAX_LENGTH) {
+            return text
+        }
+        return text.substring(0, MAX_LENGTH - END_LENGTH - 3) + "..." + text.substring(text.length - END_LENGTH)
     }
 }
--- src/jvmMain/kotlin/io/grule/util/log/SysPrinter.kt
+++ src/jvmMain/kotlin/io/grule/util/log/SysPrinter.kt
@@ -4,10 +4,19 @@ import java.text.SimpleDateFormat
 import java.util.*
 
 actual object SysPrinter : Printer {
-    private const val DATE_FORMAT = "HH:mm:ss:SSS";
+    private const val DATE_FORMAT = "HH:mm:ss:SSS"
+    private const val MAX_LENGTH = 1000
+    private const val END_LENGTH = 20
 
     override fun print(level: LogLevel, tag: String, message: Any?) {
         val date = SimpleDateFormat(DATE_FORMAT).format(Date())
-        println("[${level.name.first()}. $date #$tag] $message")
+        println("[${level.name.first()}. $date #$tag] ${shortText(message.toString())}")
+    }
+
+    private fun shortText(text: String): String {
+        if (text.length <= MAX_LENGTH) {
+            return text
+        }
+        return text.substring(0, MAX_LENGTH - END_LENGTH - 3) + "..." + text.substring(text.length - END_LENGTH)
     }
 }
